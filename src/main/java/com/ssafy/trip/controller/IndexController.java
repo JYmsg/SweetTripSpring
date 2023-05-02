57cd905c2ac3dc082b93cc83710f36e390f993a7
rladbwls30@naver.com <rladbwls30@naver.com>
2023-05-02 16:41:25 +0900

    Feat: index 연결

--- src/main/java/com/ssafy/trip/controller/IndexController.java
+++ src/main/java/com/ssafy/trip/controller/IndexController.java
@@ -1,17 +1,46 @@
 package com.ssafy.trip.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ssafy.trip.dto.HotPlace;
+import com.ssafy.trip.model.service.HotPlaceService;
 
 @Controller
 public class IndexController {
+	@Autowired
+	private HotPlaceService hs;
+	
 	@GetMapping("/")
-	public String index() {
+	public String index(Model m) throws SQLException {
+		List<HotPlace> list = hs.selectAll();
+		List<HotPlace> result = new ArrayList<HotPlace>();
+		for(HotPlace place : list) {
+			if(result.size()>=6) break;
+			result.add(place);
+		}
+		m.addAttribute("hotpls", result);
 		return "index";
 	}
 	
-	@GetMapping("/rest")
-	public String rest() {
-		return "rest";
+	@GetMapping("/hotplacelist")
+	public String hotplacelist() {
+		return "hotplace/hotplacelist";
+	}
+	
+	@GetMapping("/hotplacedetail")
+	public String hotplacedetail() {
+		return "hotpalce/hotplaceview";
+	}
+	
+	@GetMapping("/hotplaceregist")
+	public String hotplaceregist() {
+		return "hotplace/hotplaceregist";
 	}
 }
