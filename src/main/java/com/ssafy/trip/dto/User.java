15063772613c6e800c8229c49ec997b34d730c11
rladbwls30@naver.com <rladbwls30@naver.com>
2023-05-02 15:03:43 +0900

    Feat: user rest로 변경

--- src/main/java/com/ssafy/trip/dto/User.java
+++ src/main/java/com/ssafy/trip/dto/User.java
@@ -11,6 +11,8 @@ public class User {
 	private String name;
 	private String id;
 	private String password;
-	private String emailname;
-	private String domain;
+	private String salt;
+	private String email;
+	private String age;
+	private String writeArticle;
 }
